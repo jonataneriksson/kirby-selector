e7d85cb64d622cdec982368a04295ecde4a8bd17
Jonatan Eriksson <jonatan@tsto.org>
2017-09-08 20:28:38 -0400

    Set select multiple as default.

--- field/selector.php
+++ field/selector.php
@@ -31,7 +31,7 @@ class SelectorField extends BaseField
      * @var string
      * @since 1.0.0
      */
-    protected $mode = 'single';
+    protected $mode = 'multiple';
 
     /**
      * Sort mode.
@@ -96,7 +96,7 @@ class SelectorField extends BaseField
      * @since 1.0.0
      */
     protected $defaultValues = array(
-        'mode'    => 'single',
+        'mode'    => 'multiple',
         'options' => 'all',
         'size'    => 'auto',
     );
